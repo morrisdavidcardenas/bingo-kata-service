f77ed3063676bd66c4bdd93c25525a03904f468f
morrisdavidcardenas <54685908+morrisdavidcardenas@users.noreply.github.com>
2021-07-25 04:18:01 -0500

    Update card.js

--- api/controllers/card.js
+++ api/controllers/card.js
@@ -1,3 +1,4 @@
+const Number = require('./number');
 const Bingo = require('../data/bingo');
 const rules = Bingo.getCardRules();
 let listNumber = [];
@@ -25,6 +26,56 @@ exports.generate = async (req,res,next) => {
     listCard.push(card);
     result = { quantity : listCard.length, list : listCard };
     res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+}
+
+exports.check = async (req,res,next) => {
+  try {
+    let message = "";
+    let bingoListNumber = [];    
+    let bingoNumber = [];
+    list = Number.getList();
+    flagWinner = false;
+    listCard.forEach(card => {
+      bingoNumber = [];
+      card.card.forEach(element => {
+        console.log(element[0,1]);
+        //Letter B
+        bingoNumber.push(element[0,1]);
+        bingoNumber.push(element[0,2]);         
+        bingoNumber.push(element[0,3]);         
+        bingoNumber.push(element[0,4]);    
+        if (element[0] != "N") {
+          bingoNumber.push(element[0,5]);         
+        }
+      });
+      bingoListNumber.push(bingoNumber);        
+    });
+    console.log(bingoListNumber);
+    let matches = 0;
+    bingoListNumber.forEach(bingo => {
+      bingo.forEach(element => {
+        if (!list.find(x=> x == element)) {
+          return;
+        } else {
+          matches = matches + 1;
+        }
+      });
+      if (matches == 24) {
+        flagWinner = true;
+        return;
+      }
+    });
+    if (flagWinner) {
+      message = "the player is the winner";
+    } else {
+      message = "the player is not the winner";
+    }
+    res.status(200).json(message);
   } catch (error) {
     res.status(500).json({
       error: error.message
