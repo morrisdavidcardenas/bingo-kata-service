e9dbdf97ae8dee35728a855b1f71d9136aa5e40d
morrisdavidcardenas <54685908+morrisdavidcardenas@users.noreply.github.com>
2021-07-25 04:18:50 -0500

    Update card.js

--- api/routes/card.js
+++ api/routes/card.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const cardController = require('../controllers/card');
 
 router.get('/generate', cardController.generate);
+router.get('/check', cardController.check);
 
 module.exports = router;
